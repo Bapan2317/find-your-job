f8122e7540bb5465502abb90e44b372e4967db1f
Bapan2317 <bapanm2317@gmail.com>
2025-05-04 18:51:57 +0530

    project start setup

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,11 @@
+import { createBrowserRouter } from "react-router";
+
+
+export const router = createBrowserRouter(
+    [
+        {
+            path: "/",
+            element: <h1>Welcome</h1>
+        }
+    ]
+)
